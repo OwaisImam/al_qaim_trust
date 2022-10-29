0578147371ff13357551af40fed41acccc6c3d1f
Owais Imam <44465478+OwaisImam@users.noreply.github.com>
2022-10-30 03:34:46 +0500

    comment dashboard

--- resources/views/dashboard/super_admin.blade.php
+++ resources/views/dashboard/super_admin.blade.php
@@ -6,59 +6,60 @@
 
 @section('content')
 
-<div class="col-xxl-6">
+{{--<div class="col-xxl-6">--}}
 
-    <div class="row">
-        <div class="col-lg-4 col-6">
-            <div class="card" style="height: 226px">
-                <div class="card-body">
-                    <div class="theme-avtar bg-primary">
-                        <i class="ti ti-users"></i>
-                    </div>
-                    <p class="text-muted text-sm mt-4 mb-2">{{ __('Total Users') }} : {{$user->total_user}}</p>
-                    <h6 class="mb-3">{{ __('Paid Users') }}</h6>
-                    <h3 class="mb-0">{{$user['total_paid_user'] }}</h3>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-4 col-6">
-            <div class="card" style="height: 226px">
-                <div class="card-body">
-                    <div class="theme-avtar bg-info">
-                        <i class="ti ti-shopping-cart"></i>
-                    </div>
-                    <p class="text-muted text-sm mt-4 mb-2">{{ __('Total Orders') }}:{{ $user->total_orders}}</p>
-                    <h6 class="mb-3">{{ __('Total Order Amount') }}</h6>
-                    <h3 class="mb-0">{{ (!empty(env('CURRENCY_SYMBOL')) ? env('CURRENCY_SYMBOL') : '$') . $user['total_orders_price'] }}</h3>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-4 col-6">
-            <div class="card" style="height: 226px">
-                <div class="card-body">
-                    <div class="theme-avtar bg-warning">
-                        <i class="ti ti-trophy"></i>
-                    </div>
-                    <p class="text-muted text-sm mt-4 mb-2">{{ __('Total Plan') }}: {{$user['total_plan']}}</p>
-                    <h6 class="mb-3">{{ __('Most Purchase Plan') }}</h6>
-                    <h3 class="mb-0">{{ $user['most_purchese_plan'] }}</h3>
-                </div>
-            </div>
-        </div>
+{{--    <div class="row">--}}
+{{--        <div class="col-lg-4 col-6">--}}
+{{--            <div class="card" style="height: 226px">--}}
+{{--                <div class="card-body">--}}
+{{--                    <div class="theme-avtar bg-primary">--}}
+{{--                        <i class="ti ti-users"></i>--}}
+{{--                    </div>--}}
+{{--                    <p class="text-muted text-sm mt-4 mb-2">{{ __('Total Users') }} : {{$user->total_user}}</p>--}}
+{{--                    <h6 class="mb-3">{{ __('Paid Users') }}</h6>--}}
+{{--                    <h3 class="mb-0">{{$user['total_paid_user'] }}</h3>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--        <div class="col-lg-4 col-6">--}}
+{{--            <div class="card" style="height: 226px">--}}
+{{--                <div class="card-body">--}}
+{{--                    <div class="theme-avtar bg-info">--}}
+{{--                        <i class="ti ti-shopping-cart"></i>--}}
+{{--                    </div>--}}
+{{--                    <p class="text-muted text-sm mt-4 mb-2">{{ __('Total Orders') }}:{{ $user->total_orders}}</p>--}}
+{{--                    <h6 class="mb-3">{{ __('Total Order Amount') }}</h6>--}}
+{{--                    <h3 class="mb-0">{{ (!empty(env('CURRENCY_SYMBOL')) ? env('CURRENCY_SYMBOL') : '$') . $user['total_orders_price'] }}</h3>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--        <div class="col-lg-4 col-6">--}}
+{{--            <div class="card" style="height: 226px">--}}
+{{--                <div class="card-body">--}}
+{{--                    <div class="theme-avtar bg-warning">--}}
+{{--                        <i class="ti ti-trophy"></i>--}}
+{{--                    </div>--}}
+{{--                    <p class="text-muted text-sm mt-4 mb-2">{{ __('Total Plan') }}: {{$user['total_plan']}}</p>--}}
+{{--                    <h6 class="mb-3">{{ __('Most Purchase Plan') }}</h6>--}}
+{{--                    <h3 class="mb-0">{{ $user['most_purchese_plan'] }}</h3>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
 
-    </div>
-</div>
-<div class="col-xxl-6">
-    <div class="card">
-        <div class="card-header">
-            <h5>{{ __('Recent Order') }}</h5>
-         </div>
-        <div class="card-body">
-            <div id="chart-sales" height="200" class="p-3"></canvas>
-        </div>
+{{--    </div>--}}
+{{--</div>--}}
+{{--<div class="col-xxl-6">--}}
+{{--    <div class="card">--}}
+{{--        <div class="card-header">--}}
+{{--            <h5>{{ __('Recent Order') }}</h5>--}}
+{{--         </div>--}}
+{{--        <div class="card-body">--}}
+{{--            <div id="chart-sales" height="200" class="p-3"></canvas>--}}
+{{--        </div>--}}
 
-    </div>
-</div>
+{{--    </div>--}}
+{{--</div>--}}
+{{--</div>--}}
 
 
 @endsection
--- storage/framework/views/c18573ab87bffb1bc166ad336886953157569575.php
+++ storage/framework/views/c18573ab87bffb1bc166ad336886953157569575.php
@@ -6,59 +6,60 @@
 
 <?php $__env->startSection('content'); ?>
 
-<div class="col-xxl-6">
 
-    <div class="row">
-        <div class="col-lg-4 col-6">
-            <div class="card" style="height: 226px">
-                <div class="card-body">
-                    <div class="theme-avtar bg-primary">
-                        <i class="ti ti-users"></i>
-                    </div>
-                    <p class="text-muted text-sm mt-4 mb-2"><?php echo e(__('Total Users')); ?> : <?php echo e($user->total_user); ?></p>
-                    <h6 class="mb-3"><?php echo e(__('Paid Users')); ?></h6>
-                    <h3 class="mb-0"><?php echo e($user['total_paid_user']); ?></h3>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-4 col-6">
-            <div class="card" style="height: 226px">
-                <div class="card-body">
-                    <div class="theme-avtar bg-info">
-                        <i class="ti ti-shopping-cart"></i>
-                    </div>
-                    <p class="text-muted text-sm mt-4 mb-2"><?php echo e(__('Total Orders')); ?>:<?php echo e($user->total_orders); ?></p>
-                    <h6 class="mb-3"><?php echo e(__('Total Order Amount')); ?></h6>
-                    <h3 class="mb-0"><?php echo e((!empty(env('CURRENCY_SYMBOL')) ? env('CURRENCY_SYMBOL') : '$') . $user['total_orders_price']); ?></h3>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-4 col-6">
-            <div class="card" style="height: 226px">
-                <div class="card-body">
-                    <div class="theme-avtar bg-warning">
-                        <i class="ti ti-trophy"></i>
-                    </div>
-                    <p class="text-muted text-sm mt-4 mb-2"><?php echo e(__('Total Plan')); ?>: <?php echo e($user['total_plan']); ?></p>
-                    <h6 class="mb-3"><?php echo e(__('Most Purchase Plan')); ?></h6>
-                    <h3 class="mb-0"><?php echo e($user['most_purchese_plan']); ?></h3>
-                </div>
-            </div>
-        </div>
 
-    </div>
-</div>
-<div class="col-xxl-6">
-    <div class="card">
-        <div class="card-header">
-            <h5><?php echo e(__('Recent Order')); ?></h5>
-         </div>
-        <div class="card-body">
-            <div id="chart-sales" height="200" class="p-3"></canvas>
-        </div>
 
-    </div>
-</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 <?php $__env->stopSection(); ?>
